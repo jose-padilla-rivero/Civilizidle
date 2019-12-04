770f3fa14514590e947736e250c40abd4ecb3c78
Gerardo <gerardo.cardosa@gmail.com>
2019-12-03 16:56:12 -0800

    Firebase Analytics
    This commit will contain the information related to Firebase Analytics and some events added for testing. These changes are reflected on the ClickManager file.

--- Scripts/Managers/ClickManager.cs
+++ Scripts/Managers/ClickManager.cs
@@ -2,6 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 
+using Firebase;
+using Firebase.Analytics;
+using Firebase.Extensions;
+using System;
+using System.Threading.Tasks;
+
 namespace IdleClickerKit {
  
 	/// <summary>
@@ -36,6 +42,105 @@ namespace IdleClickerKit {
 		/// affects this value. It is NOT persisted.
 		/// </summary>
 		protected int clickIncrement = 1;
+
+
+
+		/// <summary>
+		///  Firebase
+		/// </summary>
+		DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
+		protected bool firebaseInitialized = false;
+		private string logText = "";
+		const int kMaxLogSize = 16382;
+		private Vector2 controlsScrollViewVector = Vector2.zero;
+		private Vector2 scrollViewVector = Vector2.zero;
+
+		public virtual void Start()
+		{
+			FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+				dependencyStatus = task.Result;
+				if (dependencyStatus == DependencyStatus.Available)
+				{
+					InitializeFirebase();
+				}
+				else
+				{
+					Debug.LogError(
+					  "Could not resolve all Firebase dependencies: " + dependencyStatus);
+				}
+			});
+		}
+
+		void InitializeFirebase()
+		{
+			DebugLog("Enabling data collection.");
+			FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+
+			DebugLog("Set user properties.");
+			// Set the user's sign up method.
+			FirebaseAnalytics.SetUserProperty(
+			  FirebaseAnalytics.UserPropertySignUpMethod,
+			  "Google");
+			// Set the user ID.
+			FirebaseAnalytics.SetUserId("uber_user_510");
+			// Set default session duration values.
+			FirebaseAnalytics.SetMinimumSessionDuration(new TimeSpan(0, 0, 10));
+			FirebaseAnalytics.SetSessionTimeoutDuration(new TimeSpan(0, 30, 0));
+			firebaseInitialized = true;
+
+			Debug.LogError( "Firebase Hell yeahhhh -======================================---------- ");
+
+			Firebase.Analytics.FirebaseAnalytics
+  .LogEvent(Firebase.Analytics.FirebaseAnalytics.EventLogin);
+
+			// Log an event with a float parameter
+			Firebase.Analytics.FirebaseAnalytics
+			  .LogEvent("progress", "percent", 0.4f);
+
+			// Log an event with an int parameter.
+			Firebase.Analytics.FirebaseAnalytics
+			  .LogEvent(
+				Firebase.Analytics.FirebaseAnalytics.EventPostScore,
+				Firebase.Analytics.FirebaseAnalytics.ParameterScore,
+				42
+			  );
+
+			// Log an event with a string parameter.
+			Firebase.Analytics.FirebaseAnalytics
+			  .LogEvent(
+				Firebase.Analytics.FirebaseAnalytics.EventJoinGroup,
+				Firebase.Analytics.FirebaseAnalytics.ParameterGroupId,
+				"spoon_welders"
+			  );
+
+			// Log an event with multiple parameters, passed as a struct:
+			Firebase.Analytics.Parameter[] LevelUpParameters = {
+  new Firebase.Analytics.Parameter(
+	Firebase.Analytics.FirebaseAnalytics.ParameterLevel, 5),
+  new Firebase.Analytics.Parameter(
+	Firebase.Analytics.FirebaseAnalytics.ParameterCharacter, "mrspoon"),
+  new Firebase.Analytics.Parameter(
+	"hit_accuracy", 3.14f)
+};
+			Firebase.Analytics.FirebaseAnalytics.LogEvent(
+			  Firebase.Analytics.FirebaseAnalytics.EventLevelUp,
+			  LevelUpParameters);
+		}
+
+		// Output text to the debug log text field, as well as the console.
+		public void DebugLog(string s)
+		{
+			print(s);
+			logText += s + "\n";
+
+			while (logText.Length > kMaxLogSize)
+			{
+				int index = logText.IndexOf("\n");
+				logText = logText.Substring(index + 1);
+			}
+
+			scrollViewVector.y = int.MaxValue;
+		}
 
 		/// <summary>
 		/// If there is already a manager destroy self, else initialise and assign to the static reference.
